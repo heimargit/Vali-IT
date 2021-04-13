fab3085e3e6954575b67a0554bc47e0d6d093a8d
Margit Loo <margit_loo@hotmail.com>
2021-04-13 22:34:13 +0300

    12.04.2021 13.04.2012

--- src/main/java/ee/bcs/valiit/codewars/eight_1.java
+++ src/main/java/ee/bcs/valiit/codewars/eight_1.java
@@ -2,14 +2,15 @@ package ee.bcs.valiit.codewars;
 
 public class eight_1 {
     public static void main(String[] args) {
-//        Ask a small girl -"How old are you?".She always says strange things...Lets help her !
-//        For correct answer program should return int from 0 to 9.
-//        Assume test input string always valid and may look like "1 year old" or "5 years old", etc..The first char is
-//        number only.
-//        Ülesanne siin: https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/java
 
-        //ehk tagasta Stringi esimene character int-kujul
     }
+
+    /*
+    Ask a small girl -"How old are you?".She always says strange things...Lets help her !
+    For correct answer program should return int from 0 to 9.
+    Assume test input string always valid and may look like "1 year old" or "5 years old", etc..The first char is number only.
+    Ülesanne siin: https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/java
+     */
 
     public static int howOld(final String herOld) {
 
