06020ff2385c01b33e501b59bef379a525048593
Margit Loo <margit_loo@hotmail.com>
2021-04-15 12:08:04 +0300

    Employee class & Postman first time

---
+++ src/main/java/ee/bcs/valiit/lessons/CreateBankAccount.java
@@ -0,0 +1,23 @@
+package ee.bcs.valiit.lessons;
+
+public class CreateBankAccount {
+
+    private String accountNr;
+    private Double balance;
+
+    public String getAccountNr() {
+        return accountNr;
+    }
+
+    public void setAccountNr(String accountNr) {
+        this.accountNr = accountNr;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
